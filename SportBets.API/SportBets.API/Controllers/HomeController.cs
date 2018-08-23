5ca525be2ee321d3d92f6765873f126eef406ebd
PurrfectME <it.efr99@gmail.com>
2018-08-23 14:44:00 +0300

    new test branch

---
+++ SportBets.API/SportBets.API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportBets.BLL.Entities;
+
+namespace SportBets.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public int Index()
+        {
+            return 100;
+        }
+    }
+}
